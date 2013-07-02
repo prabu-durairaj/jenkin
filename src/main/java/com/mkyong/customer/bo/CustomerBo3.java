762219614942a2418a0d181eaf0e29e5ae8995f9
prabu <prabu.d@solverminds.com>
2013-07-02 11:41:31 +0530

    added java file

---
+++ src/main/java/com/mkyong/customer/bo/CustomerBo3.java
@@ -0,0 +1,13 @@
+package com.mkyong.customer.bo;
+
+import java.util.List;
+
+import com.mkyong.customer.model.Customer;
+ 
+public interface CustomerBo3{
+ 
+  void addCustomer(Customer customer);
+ 
+	List<Customer> findAllCustomer();
+ 
+}
